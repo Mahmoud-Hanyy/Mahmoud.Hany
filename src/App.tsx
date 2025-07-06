db400c7b56ed4eb8f6a370aeabf612c77b74286a
Mahmoud <mahmoudhany374@gmail.com>
2025-07-06 18:01:42 +0300

    fix: Deployment bugs fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Header } from './components/Header/Header'
-import { Main } from './components/Main/Main'
-import { GlobalStyle } from './styles/global'
-import { Analytics } from "@vercel/analytics/react"
-import { Footer } from './components/Footer/Footer'
-import 'react-toastify/dist/ReactToastify.css'
+import React from 'react';
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+import { GlobalStyle } from './styles/global';
+import { inject } from '@vercel/analytics';
+import { Footer } from './components/Footer/Footer';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
+  inject();
   return (
     <>
-      <GlobalStyle></GlobalStyle>
-      <Header></Header>
-      <Main></Main>
-      <Analytics />
-      <Footer></Footer>
+      <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
